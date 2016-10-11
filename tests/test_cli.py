609fa02c8edb80496f477bd6fb1a849b7cb1a793
Rob McBroom <rob@sixfeetup.com>
2016-10-11 14:19:39 -0400

    add tests for the report sub-command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -228,3 +228,49 @@ class LogTest(BaseTest):
             ['custodian', 'logs', '-c', nl.name, '-s', temp_dir]
         )
         cli.main()
+
+
+class ReportTest(BaseTest):
+
+    def test_report(self):
+        valid_policies = {
+            'policies':
+            [{
+                'name': 'foo',
+                'resource': 's3',
+                'filters': [{"tag:custodian_tagging": "not-null"}],
+                'actions': [{'type': 'tag', 'tags': ['custodian_cleanup']}],
+            }]
+        }
+        v = tempfile.NamedTemporaryFile(suffix=".yml")
+        v.write(yaml.dump(valid_policies, Dumper=yaml.SafeDumper))
+        v.flush()
+        self.addCleanup(v.close)
+
+        temp_dir = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, temp_dir)
+        exit_code = []
+
+        def exit(code):
+            exit_code.append(code)
+
+        self.patch(sys, 'exit', exit)
+        self.patch(
+            sys,
+            'argv',
+            ['custodian', 'report', '-c', v.name, '-s', temp_dir]
+        )
+        cli.main()
+        self.assertEqual(exit_code, [])
+
+        # empty file
+        e = tempfile.NamedTemporaryFile(suffix=".yml")
+        e.write(yaml.dump({'policies': []}, Dumper=yaml.SafeDumper))
+        e.flush()
+        self.addCleanup(e.close)
+        self.patch(
+            sys,
+            'argv',
+            ['custodian', 'logs', '-c', e.name, '-s', temp_dir]
+        )
+        self.assertRaises(AssertionError, cli.main)
